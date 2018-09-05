d31c763c888ff48265a1e6bff47b3dabb388507d
nnaemeka obioha <nnaemeka.obioha@gmail.com>
2018-09-05 15:13:44 +0100

    error fix commit

--- server/server.js
+++ server/server.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const path  = require('path');
-const app  = express();
+
 
 
 //Static file
-const publicPath = path.join(__dirname + '../public');
-app.use(express.static(publicPath))
-
+const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
+const app  = express();
+
+app.use(express.static(publicPath));
 app.listen(port, () => {
     console.log('App listening on port 3000!');
 });

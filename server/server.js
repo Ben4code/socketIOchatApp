737a1593f9eb5a523f61bd2ec35c0ae8132c233a
nnaemeka obioha <nnaemeka.obioha@gmail.com>
2018-09-06 14:25:32 +0100

    added index.js script file

--- server/server.js
+++ server/server.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const path  = require('path');
+const http = require('http');
+const socketIO = require('socket.io');
 
 
 
 //Static file
 const publicPath = path.join(__dirname, '../public');
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 const app  = express();
+let server = http.createServer(app);
+
 
 app.use(express.static(publicPath));
-app.listen(port, () => {
+server.listen(PORT, () => {
     console.log('App listening on port 3000!');
 });
